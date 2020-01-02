9b281ba6324d7552e77b202bf4eb6a271a0e9098
m2nlight <wingingbob@gmail.com>
2020-01-02 22:37:11 +0800

    formatted build.sh

--- build.sh
+++ build.sh
@@ -15,60 +15,60 @@ package=''
 declare -i verbose=0
 
 until [ $# -eq 0 ]; do
-	case "$1" in
-	--help|-h)
-		printf "usage: bash ${0##*/} [--verbose|-v] [--package|-p <package-name>] [--name|-n <app-name>] [--output|-o <output-path>]\n"
-		exit 0
-		;;
-  --verbose|-v)
-    verbose=1
-    ;;
-	--package|-n)
-		shift
-		if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
-			printf "ERROR: package argument error: $1\n"
-			exit 1
-		fi
-		package=$1
-		;;
-	--name|-n)
-		shift
-		if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
-			printf "ERROR: name argument error: $1\n"
-			exit 1
-		fi
-		appname=$1
-		;;
-	--output|-o)
-		shift
-		if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
-			printf "ERROR: output argument error: $1\n"
-			exit 1
-		fi
-		outputpath=$1
-		;;
-	*)
-		printf "ERROR: arguments error. please run \"bash ${0##*/} --help\" to get usage\n"
-		exit 1
-		;;
-	esac
-	shift
+    case "$1" in
+        --help|-h)
+            printf "usage: bash ${0##*/} [--verbose|-v] [--package|-p <package-name>] [--name|-n <app-name>] [--output|-o <output-path>]\n"
+            exit 0
+        ;;
+        --verbose|-v)
+            verbose=1
+        ;;
+        --package|-n)
+            shift
+            if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
+                printf "ERROR: package argument error: $1\n"
+                exit 1
+            fi
+            package=$1
+        ;;
+        --name|-n)
+            shift
+            if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
+                printf "ERROR: name argument error: $1\n"
+                exit 1
+            fi
+            appname=$1
+        ;;
+        --output|-o)
+            shift
+            if [ -z "$1" ] || [[ "$1" =~ ^\- ]]; then
+                printf "ERROR: output argument error: $1\n"
+                exit 1
+            fi
+            outputpath=$1
+        ;;
+        *)
+            printf "ERROR: arguments error. please run \"bash ${0##*/} --help\" to get usage\n"
+            exit 1
+        ;;
+    esac
+    shift
 done
 
 if [ -z "$appname" ]; then
-	printf "ERROR: please input an app-name by --name\n"
-	exit 2
+    printf "ERROR: please input an app-name by --name\n"
+    exit 2
 fi
 
 if [ -z "$outputpath" ]; then
-	printf "ERROR: please input an output-path by --output\n"
-	exit 2
+    printf "ERROR: please input an output-path by --output\n"
+    exit 2
 fi
 
 mkdir -p "$outputpath"
 if [ $? -ne 0 ]; then
-	printf "ERROR: create $outputpath failed: $?\n"
-	exit 2
+    printf "ERROR: create $outputpath failed: $?\n"
+    exit 2
 fi
 
 cat << EOF
@@ -78,41 +78,41 @@ Output: $outputpath
 EOF
 
 if [ ! -f go.mod ]; then
-  printf "\nCreating go.mod ...\n"
-  go mod init $appname
-  if [ $? -ne 0 ]; then
-    printf "Error: $?\n"
-    exit 2
-  fi
+    printf "\nCreating go.mod ...\n"
+    go mod init $appname
+    if [ $? -ne 0 ]; then
+        printf "Error: $?\n"
+        exit 2
+    fi
 fi
 
 declare -i count=${#GOOS_GOARCH_array[*]}
 declare -i num=0
 for target in "${GOOS_GOARCH_array[@]}"; do
-	let num+=1
-	output=$outputpath/${appname}_${target}
-  mygoos=${target%%_*}
-  mygoarch=${target##*_}
-  myverbose=''
-  mycgo=''
-  if [ $verbose -eq 1 ]; then
-    myverbose='-v'
-  fi
-  case "$mygoos" in
-    windows)
-		  output="$output.exe"
-      ;;
-    android)
-      mycgo='CGO_ENABLED=1'
-      ;;
-  esac
-	printf "\n($num/$count) Building $output ...\n"
-	env GOOS=$mygoos GOARCH=$mygoarch `printf "$mycgo"` go build $myverbose -ldflags "-s -w" -o $output $package
-	file $output
+    let num+=1
+    output=$outputpath/${appname}_${target}
+    mygoos=${target%%_*}
+    mygoarch=${target##*_}
+    myverbose=''
+    mycgo=''
+    if [ $verbose -eq 1 ]; then
+        myverbose='-v'
+    fi
+    case "$mygoos" in
+        windows)
+            output="$output.exe"
+        ;;
+        android)
+            mycgo='CGO_ENABLED=1'
+        ;;
+    esac
+    printf "\n($num/$count) Building $output ...\n"
+    env GOOS=$mygoos GOARCH=$mygoarch `printf "$mycgo"` go build $myverbose -ldflags "-s -w" -o $output $package
+    file $output
 done
 
 if [ -t 1 ]; then
-	printf "\nDone. Press any key to exit..."
-	read -n1
-	printf "\n"
+    printf "\nDone. Press any key to exit..."
+    read -n1
+    printf "\n"
 fi
